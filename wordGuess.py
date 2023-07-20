74d9845055737e723592c15a66f297eeaec77420
nehag1165 <62196771+nehag1165@users.noreply.github.com>
2023-07-20 16:03:33 +0530

    Guess the random word out of a collection of words

---
+++ wordGuess.py
@@ -0,0 +1,38 @@
+import random
+
+name = input("what is your name? ")
+print("Welcome "+name+" Guess the alphabates ")
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks', 'girls', 'comma']
+
+word = random.choice(words)
+
+turn = 12
+guess = ""
+
+while turn > 0:
+
+    failed = 0
+    for char in word:
+        if char in guess:
+            print(char)
+        else:
+            print('_')
+            failed += 1
+
+    if failed == 0:
+        print("You Win!!!")
+        print("the guess "+guess+" is found in ",turn,"tries ")
+
+    a = input('guess a character: ')
+    guess += a
+
+    if a not in word:
+        print("wrong ")
+        turn -= 1
+        print("you have ",turn,"turns left ")
+
+        if turn == 0:
+            print("you lose ")
